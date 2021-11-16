672b4d266e43020bb477df96584af090d01542d1
Juanra08 <juanra08.jrpn@gmail.com>
2021-11-16 12:08:38 +0100

    creo el formulario para añadir trabajos

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -23,7 +23,7 @@
 		modelo:"",
 		año:""
 	}
-	
+
 	let coches = [];
 
 	// Cargamos datos de bd
@@ -51,6 +51,84 @@
 </script>
 
 <main>
+
+	<!--Formulario trabajos-->
+
+
+
+	<!---->
+
+	<!--Formulario coches-->
+
+	<h3>Añadir trabajos</h3>
+	<div class="container flex justify-center mx-auto">
+
+	<div id="form" >
+		<form class="w-full max-w-sm">
+			<!-- bind:value=variable cada cambio del input se sincorniza con la variable previamente declarada en el código -->
+			<div class="md:flex md:items-center mb-6">
+				<div class="md:w-1/3">
+					<label  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="nombre">Trabajos</label>
+				</div>
+
+				<div class="md:w-2/3">
+					<input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+						bind:value={trabajo.nombre}
+						id="nombre"
+						type="text"
+						placeholder="Nombre del producto"
+						required
+					/>
+				</div>
+			</div>
+
+			<div class="md:flex md:items-center mb-6">
+				<div class="md:w-1/3">
+					<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="descripcion">Descripción</label>
+				</div>
+
+				<div class="md:w-2/3">
+					<textarea class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+						bind:value={trabajo.descripcion}
+						id="descripcion"
+						rows="3"
+						placeholder="Descripción del producto"
+						required
+					/>
+				</div>
+			</div>
+			<div class="md:flex md:items-center mb-6">
+				<div class="md:w-1/3">
+					<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="select">Selecciona el coche</label>
+				</div>
+				<div class="md:w-1/3">
+					<select bind:value={trabajo.id_coche} id="vehiculo" required>
+
+					</select>
+				</div>
+			</div>
+			<div class="md:flex md:items-center">
+				<div class="md:w-1/3"></div>
+				<div class="md:w-2/3">
+					<button class="shadow bg-green-500 hover:bg-green-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" on:click={mostrarFormulario}>Añadir coche</button>
+				</div>
+			</div>
+			<div class="md:flex md:items-center">
+				<div class="md:w-1/3"></div>
+				<div class="md:w-2/3">
+						<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">Editar</button>
+						<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">Añadir</button>
+
+					
+				</div>
+			</div>
+
+		</form>
+	</div>
+	</div>
+
+	<!---->
+
 
 </main>
 
