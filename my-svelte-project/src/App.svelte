ea9f189ca3511a30d7df772654170be8c2fd3504
Juanra08 <juanra08.jrpn@gmail.com>
2021-11-16 12:12:07 +0100

    añado el formulario para introducir coches

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -54,12 +54,6 @@
 
 	<!--Formulario trabajos-->
 
-
-
-	<!---->
-
-	<!--Formulario coches-->
-
 	<h3>Añadir trabajos</h3>
 	<div class="container flex justify-center mx-auto">
 
@@ -129,6 +123,75 @@
 
 	<!---->
 
+	<!--Formulario coches-->
+	<div class="container flex justify-center mx-auto">
+
+		<div style="display: none; " id="formCoche">
+			<form class="w-full max-w-sm">
+	
+				<div class="md:flex md:items-center mb-6">
+					<div class="md:w-1/3">
+						<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="marca">Marca</label>
+					</div>
+					<div class="md:w-2/3">
+						<input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+							bind:value={coche.marca}
+							id="marca"
+							type="text"
+							placeholder="Nombre del producto"
+							required
+						/>
+					</div>
+				</div>
+	
+				<div class="md:flex md:items-center mb-6">
+					<div class="md:w-1/3">
+						<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="modelo">Modelo</label>
+					</div>
+					<div class="md:w-2/3">
+						<input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+							bind:value={coche.modelo}
+							id="modelo"
+							type="text"
+							placeholder="Nombre del producto"
+							required
+						/>
+					</div>
+				</div>
+				
+				<div class="md:flex md:items-center mb-6">
+					<div class="md:w-1/3">
+						<label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="year">Año</label>
+					</div>
+					<div class="md:w-2/3">			
+						<input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+							bind:value={coche.año}
+							id="año"
+							type="text"
+							placeholder="Nombre del producto"
+							required
+						/>
+					</div>
+				</div>
+				<div class="md:flex md:items-center">
+					<div class="md:w-1/3"></div>
+					<div class="md:w-2/3">
+						<button class="shadow bg-red-500 hover:bg-red-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" on:click={cancelar}>Cancelar</button>
+					</div>
+				</div>	
+				
+				<div class="md:flex md:items-center">
+					<div class="md:w-1/3"></div>
+					<div class="md:w-2/3">
+						<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">Añadir</button>
+					</div>
+				</div>	
+		
+			</form>
+		</div>
+		</div>
+	
+	<!---->
 
 </main>
 
